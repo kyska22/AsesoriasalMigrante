85705d8e81447d89df0bb544bb02c401e602bb7c
Kyska Harrington <129115993+kyska22@users.noreply.github.com>
2026-05-21 12:19:00 -0300

    agregando profesionalismo al email

--- scripts.js
+++ scripts.js
@@ -10,3 +10,37 @@ const nav = document.getElementById("nav");
 menuToggle.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+const formulario = document.getElementById("formulario-contacto");
+const mensaje = document.getElementById("mensaje-exito");
+
+formulario.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const datos = new FormData(formulario);
+  try {
+    const respuesta = await fetch(formulario.action, {
+      method: "POST",
+      body: datos,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    if (respuesta.ok) {
+      mensaje.textContent = "Consulta enviada correctamente.";
+      mensaje.classList.add("activo");
+      formulario.reset();
+      setTimeout(() => {
+        mensaje.classList.remove("activo");
+      }, 4000);
+    } else {
+      mensaje.textContent = "Ocurrió un error. Intenta nuevamente.";
+      mensaje.classList.add("activo");
+    }
+  } catch (error) {
+    mensaje.textContent = "Error de conexión.";
+
+    mensaje.classList.add("activo");
+  }
+});
